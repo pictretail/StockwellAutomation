41f828063d9afc9c6d7e8264efedf90e9748b756
Padma365 <Padmavathi.d@365smartshop.com>
2024-07-12 13:56:32 +0530

    Added a testcase

--- qa-stockwell/src/test/java/org/stockwell/browser/Factory.java
+++ qa-stockwell/src/test/java/org/stockwell/browser/Factory.java
@@ -19,20 +19,32 @@ public class Factory {
 		try {
 			DesiredCapabilities capabilities = null;
 				if (browser.equals(Constants.CHROME)) {
-					capabilities = new DesiredCapabilities();
-					ChromeOptions chromeOptions = new ChromeOptions();
-					//chromeOptions.addArguments("--remote-allow-origins=*");
-					chromeOptions.addArguments("--start-maximized");
-					Map<String, Object> chromePrefs = new HashMap<>();
-					chromePrefs.put("credientials enable service", false);
-					chromeOptions.setExperimentalOption("prefs", chromePrefs);
-					chromeOptions.addArguments("--disable-plugins", "--disable-extensions", "--disable.popup.blocking");
-					capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-					capabilities.setCapability("applicationCacheEnabled", false);
-					chromeOptions.merge(capabilities);
-					//System.setProperty("webdriver.chrome.driver", FilePath.DRIVER_CHROME);
-					chromeOptions.addArguments("--disable-plugins", "--disable-extensions", "--disable.popup.blocking");
-					webDriver.set(new ChromeDriver(chromeOptions));
+					// capabilities = new DesiredCapabilities();
+					// ChromeOptions chromeOptions = new ChromeOptions();
+					// //chromeOptions.addArguments("--remote-allow-origins=*");
+					// chromeOptions.addArguments("--start-maximized");
+					// Map<String, Object> chromePrefs = new HashMap<>();
+					// chromePrefs.put("credientials enable service", false);
+					// chromeOptions.setExperimentalOption("prefs", chromePrefs);
+					// chromeOptions.addArguments("--disable-plugins", "--disable-extensions", "--disable.popup.blocking");
+					// capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+					// capabilities.setCapability("applicationCacheEnabled", false);
+					// chromeOptions.merge(capabilities);
+					// //System.setProperty("webdriver.chrome.driver", FilePath.DRIVER_CHROME);
+					// chromeOptions.addArguments("--disable-plugins", "--disable-extensions", "--disable.popup.blocking");
+					// webDriver.set(new ChromeDriver(chromeOptions));
+
+					ChromeOptions cap = new ChromeOptions();
+            cap.addArguments("--no-sandbox");
+            cap.addArguments("enable-automation");
+            cap.addArguments("--disable-gpu");
+            cap.addArguments("--headless"); // Enable headless mode
+            cap.addArguments("--disable-extensions");
+            cap.addArguments("--use-gl=swiftshader");
+            cap.addArguments("--disable-dev-shm-usage");
+            WebDriver driver = new ChromeDriver(cap);
+            //webdriver.set(new RemoteWebDriver(url,cap));
+            webDriver.set(driver);
 					 
 				} 
 		} catch (Exception exc) {
